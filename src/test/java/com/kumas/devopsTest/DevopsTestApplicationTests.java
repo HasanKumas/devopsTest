eadbe5173d7639f2fb03632b912c116a45f82577
HAKUMAS <hsnkms75@gmail.com>
2021-02-10 15:07:52 +0100

    Initial commit

---
+++ src/test/java/com/kumas/devopsTest/DevopsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumas.devopsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
